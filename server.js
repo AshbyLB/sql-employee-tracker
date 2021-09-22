02172bf00c011805624ba4cbd69c6fa77ae08545
Ashby Blakely <ashbyleeblakely@gmail.com>
2021-09-21 21:54:09 -0400

    Added more to the server.js and prompts to add role and add dept

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const express = require('express');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 const db = mysql.createConnection(
     {
-        user:'root',
+        user: 'root',
         host: 'localhost',
         password: '',
-        database:'employee_db'
+        database: 'employee_db'
     },
     console.log('Created connecton to the employee database')
 );
+
+beginPrompts();
+
 function beginPrompts() {
     inquirer.prompt([
         {
             name: 'initialPrompts',
-            type: 'rawlist',
-            message: 'Please select from theh following options:',
+            type: 'list',
+            message: 'Please select from the following options:',
             choices: [
                 'View All Departments',
                 'View All Roles',
@@ -30,19 +27,111 @@ function beginPrompts() {
                 'Add a Department',
                 'Add a Role',
                 'Add an Employee',
-                'Update an Employee Role'
+                'Update an Employee Role',
+                'Quit'
             ]
         }
-    ])
+    ]).then(function (data) {
+        if (data.initialPrompts === 'View All Departments') {
+            viewDepts();
+        } else if (data.initialPrompts === 'View All Roles') {
+            viewRoles();
+        } else if (data.initialPrompts === 'View All Employees') {
+            viewEmps();
+        } else if (data.initialPrompts === 'Add a Department') {
+            addDept();
+        } else if (data.initialPrompts === 'Add a Role') {
+            addRole();
+        } else if (data.initialPrompts === 'Add an Employee') {
+            addEmp();
+        } else if (data.initialPrompts === 'Update an Employee Role') {
+            updateEmp();
+        } else {
+            console.log('Goodbye');
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
+
+};
+
+function viewDepts() {
+    db.query(`SELECT * FROM department`, function (err, res) {
+        if (err) return console.log(err);
+        console.table(res);
+        beginPrompts();
+    });
+};
+
+function viewRoles() {
+    db.query(`SELECT * FROM role`, function (err, res) {
+        if (err) return console.log(err);
+        console.table(res);
+        beginPrompts();
+
+    });
+};
+
+function viewEmps() {
+    db.query(`SELECT * FROM employee`, function (err, res) {
+        if (err) return console.log(err);
+        console.table(res);
+        beginPrompts();
+    });
+};
+
+function addDept() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newDepartment',
+            message: 'Please provide the name of the new department.'
+        }
+    ]).then(function (data) {
+        db.query(`INSERT INTO department (name) VALUES (?)`, data.newDepartment, function (err, res){
+            if (err) return console.log(err);
+            console.table(res);
+            console.table(department);
+            beginPrompts();
+        })
+    })
 }
-db.query(`SELECT * FROM`, function (err, results) {
-    if (err) return console.log(err);
-     console.log(res.json(results));
-});
 
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+function addRole() {
+    db.query(`SELECT * FROM department`, function (err, res) {
+        if (err) return console.log(err);
+    
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'newRole',
+                message: 'Please provide new role\'s title.'
+            },
+            {
+                type: 'input',
+                name: 'newSalary',
+                message: 'Please provide the salayr for this role.' 
+            },
+            {
+                type: 'list',
+                name: 'deptId',
+                message: 'Please choose the department in which to add the new role.' 
+                choices: function DeptId () {
+                    newArr = [];
+                    for(let i = 0; i < res.length; i++){
+                        newArr.push(res[i].name);
+                    }
+                    return newArr;
+                }
+            }
+        ]).then(function (data) {
+            db.query(`INSERT INTO role (title, salery, department_id) VALUES (?,?,?)`, data.deptID, function (err, res){
+                if (err) return console.log(err);
+                console.table(res);
+                console.table(department);
+                beginPrompts();
+            })
+        })
+    });
+    }
+    
